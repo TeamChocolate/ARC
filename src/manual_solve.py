bed5f995dd55289a815211bfc275b3268d70ce31
Martin O' Gorman <info@blahsoup.com>
2020-12-19 00:34:32 +0000

    Solved 1cf80156 aka Task 30
    First task solved correctly for all cases in 1cf80156.json

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,24 +1,60 @@
 #!/usr/bin/python
 
+# Martin O' Gorman
+# 09077324
+# https://github.com/TeamChocolate/ARC
+
 import os, sys
 import json
 import numpy as np
 import re
 
-### YOUR CODE HERE: write at least three functions which solve
-### specific tasks by transforming the input x and returning the
-### result. Name them according to the task ID as in the three
-### examples below. Delete the three examples. The tasks you choose
-### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
+def solve_1cf80156(x):
+    # Task 30/400, 1cf80156
+    # Dimensions (rows, columns)
+    # Demo 1: (10, 12) -> (4, 4)
+    # Demo 2: (11, 12) -> (5, 3)
+    # Demo 3: (12, 12) -> (3, 5)
+    # Test input: (12, 12) -> (4, 6)
+    # Input/Output 2D NumPy array
+    
+    # Verbal description of the required transformation:
+    # Task 30 aka 1cf80156.json, is a task where the demonstration makes clear that grid is resized to match the the max height and width of       the shape, so in order to be able to solve the task, one must constrain the grid size to the size of the shape.
+    # To do this, I found and tracked the first/last row when colours appeared, and the first/last column where colours appeared
+    # Using the 4 captured values above, I was able to slice the NumPy array from the input parameter to correctly size the grid.
+    # No other transformations were required for the shape for a correct solution
+    
+    # Which training and test grids are solved correctly:
+    # ALL training and test grids are solved correctly for the Task 30 / 1cf80156.json
+            
+    first_row_where_data = 0
+    last_row_where_data = 0
+    last_column_where_colour = 0
+    first_column_where_colour = 99
+    
+    # index[0], index[1] gives coordinates of the pixel in the grid
+    # loop through every coordinate in the input grid
+    for index, val in np.ndenumerate(x):
+        # if not black aka 0
+        if(val != 0):            
+            row = index[0]
+            column = index[1]
+            if(first_row_where_data == 0):
+                first_row_where_data = row 
+            if(row > last_row_where_data):
+                last_row_where_data = row
+            if(column > last_column_where_colour):
+                last_column_where_colour = column
+            if(column < first_column_where_colour):
+                first_column_where_colour = column
+                
+    last_row_where_data = last_row_where_data + 1
+    last_column_where_colour = last_column_where_colour + 1
+    
+    # splice the input grid using the values obtained above into the correct output grid
+    x = x[first_row_where_data:last_row_where_data, first_column_where_colour:last_column_where_colour]       
+    
     return x
-
-def solve_b2862040(x):
-    return x
-
-def solve_05269061(x):
-    return x
-
 
 def main():
     # Find all the functions defined in this file whose names are
@@ -66,7 +102,7 @@ def read_ARC_JSON(filepath):
 def test(taskID, solve, data):
     """Given a task ID, call the given solve() function on every
     example in the task data."""
-    print(taskID)
+    print("TaskID is: ", taskID)
     train_input, train_output, test_input, test_output = data
     print("Training grids")
     for x, y in zip(train_input, train_output):
